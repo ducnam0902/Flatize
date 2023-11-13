6a7eeb0f224430974e2c46e634ea17e477fa4516
nam <70477669+ducnam0902@users.noreply.github.com>
2023-11-13 15:40:53 +0000

    Feature | Footer (#4)

---
+++ src/components/Footer/components/Tag.tsx
@@ -0,0 +1,36 @@
+import { Box } from '@mui/material';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+interface ITag {
+  title: string;
+  link: string;
+}
+
+const Tag: React.FC<ITag> = ({ title, link }) => {
+  return (
+    <Box
+      component={Link}
+      to={link}
+      sx={{
+        color: '#999999',
+        border: '1px solid #484848',
+        margin: '0 0.2rem 0.5rem 0',
+        padding: '0.5rem 1rem',
+        textTransform: 'capitalize',
+        transition: 'all 0.3s',
+        fontSize: '1.3rem',
+        lineHeight: '1.6',
+        textDecoration: 'none',
+        '&:hover': {
+          color: '#1abc9c',
+          borderColor: '#1abc9c',
+        },
+      }}
+    >
+      {title}
+    </Box>
+  );
+};
+
+export default Tag;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -20,3 +20,73 @@ export const megaMenu = [
     link: '/about',
   },
 ];
+
+export const usefullLinks = [
+  {
+    title: 'Track Orders',
+    link: '/',
+  },
+  {
+    title: 'FAQs',
+    link: '/',
+  },
+  {
+    title: 'Services',
+    link: '/',
+  },
+  {
+    title: 'Free Credit Report',
+    link: '/',
+  },
+  {
+    title: 'Terms and Conditions',
+    link: '/',
+  },
+  {
+    title: 'Privacy Policy',
+    link: '/',
+  },
+  {
+    title: 'Community Guidelines',
+    link: '/',
+  },
+];
+
+export const tags = [
+  {
+    title: 'Image',
+    link: '/',
+  },
+  {
+    title: 'Features',
+    link: '/',
+  },
+  {
+    title: 'Gallery',
+    link: '/',
+  },
+  {
+    title: 'Post Formats',
+    link: '/',
+  },
+  {
+    title: 'Typography',
+    link: '/',
+  },
+  {
+    title: 'WooCommerce',
+    link: '/',
+  },
+  {
+    title: 'Shortcodes',
+    link: '/',
+  },
+  {
+    title: 'Best Sellers',
+    link: '/',
+  },
+  {
+    title: 'Slideshow',
+    link: '/',
+  },
+];
