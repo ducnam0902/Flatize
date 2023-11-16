f2744eb6a6d44c45f6dbdc1dbe462945dd7b197b
ducnam0902 <ducnam0902@gmail.com>
2023-11-16 12:16:49 +0000

    Feature | AdsEvent

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,13 @@
-import Slide from '@flatize/components/Slide/Slide';
 import React from 'react';
+
+import Slide from '@flatize/components/Slide/Slide';
+import AdsEvent from './components/AdsEvent';
 
 const HomePage: React.FC = () => {
   return (
     <div>
       <Slide />
+      <AdsEvent />
     </div>
   );
 };
---
+++ src/pages/HomePage/components/AdsEvent.tsx
@@ -0,0 +1,30 @@
+import { Container } from '@mui/material';
+import Box from '@mui/material/Box';
+import Image from 'mui-image';
+import React from 'react';
+import ad1 from '@flatize/assets/ad-1.png';
+import ad2 from '@flatize/assets/ad-2.png';
+import ad3 from '@flatize/assets/ad-3.png';
+const AdsEvent: React.FC = () => {
+  return (
+    <Box
+      sx={{
+        background: 'whitesmoke',
+        borderBottom: '1px solid #dddddd',
+        marginBottom: '1rem',
+      }}
+    >
+      <Container
+        disableGutters
+        maxWidth="xl"
+        sx={{ display: 'flex', flexDirection: { xs: 'column', md: 'row' }, gap: '3rem', paddingY: '5rem' }}
+      >
+        <Image src={ad1} duration={0} />
+        <Image src={ad2} duration={0} />
+        <Image src={ad3} duration={0} />
+      </Container>
+    </Box>
+  );
+};
+
+export default AdsEvent;
